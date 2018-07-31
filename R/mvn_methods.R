ee6dcd5df58ae80bcb49168944b6f7df19c0c444
hkasahar <hkasahar@gmail.com>
2018-07-31 10:31:59 +0900

    changing again from mean = to mu =
    rmvnorm is a tricky name to use. Both mixtools package and mvtnorm package have the function named rmvnorm but in mvtnorm uses ``mean='' while mixtools uses ``mu='' for its argument.

--- R/mvn_methods.R
+++ R/mvn_methods.R
@@ -17,7 +17,7 @@ rmvnmix <- function(n, alpha, mu, sigma){
     nj <- sum(Ind==j)
     muj <- mu[,j]
     sigmaj <- sigma[,(d*(j-1)+1):(d*j)]
-    yj <- rmvnorm(nj, mean = muj, sigma = sigmaj)
+    yj <- rmvnorm(nj, mu = muj, sigma = sigmaj)
     y[Ind==j,] <- yj
   }
 y
